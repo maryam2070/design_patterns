915a02e10fd04542e8fa2d99383c0cefb445654e
Maryam <Mariam_Azouz@khales.com.eg>
2024-12-15 11:05:49 +0200

    clear maryam's solutions

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,9 @@
-import builder.ChickenSandwich;
-import chicken_sandwich.*;
-import singlton.FriedChickenSandwichSingleton;
-
 import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        final ChickenSandwich.ChickenSandwichBuilder builder = new ChickenSandwich.ChickenSandwichBuilder();
-        builder.setBun(Bun.FLATBREAD);
-        builder.setCheese(Cheese.AMERICAN);
-        builder.setChicken(FriedChicken.BONELESS);
-        builder.setSauce(Sauce.NO_SAUCE);
-        java.util.List<SandwichExtras> extras = new ArrayList<>();
-        extras.add(SandwichExtras.AVOCADO);
-        extras.add(SandwichExtras.BACON);
-        builder.setExtras(extras);
-        final ChickenSandwich sandwich=builder.build();
-        System.out.println(sandwich.toString());
-
-        ChickenSandwich singletonSandwich= FriedChickenSandwichSingleton.get();
-        System.out.println(singletonSandwich.toString());
 
     }
 }
